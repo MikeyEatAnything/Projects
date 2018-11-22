82dcc4ad4ba1d5af7cd57849a75fd8806bebcd09
MikeyEatAnything <mbalexan9@gmail.com>
2018-11-22 09:38:17 -0800

    Update Menu.ino

--- Arduino/Menu/Menu/Menu.ino
+++ Arduino/Menu/Menu/Menu.ino
@@ -21,6 +21,8 @@ void setup() {
   // put your setup code here, to run once:
   lcd.begin(16, 2);
   Serial.begin(9600);
+  ir_receiver.enableIRIn();
+  ir_receiver.blink13(true);
   lcd.setCursor(0,0);
   lcd.print("Welcome");
   delay(1000);
